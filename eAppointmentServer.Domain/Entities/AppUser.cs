a87eeeebb004fc7488ea7565515de7476761ae1a
Furkan Aydemir <furkanaydemir12@hotmail.com>
2025-08-01 22:01:54 +0300

    Add project files.

---
+++ eAppointmentServer.Domain/Entities/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eAppointmentServer.Domain.Entities
+
+//başka bir classın bunu implement etmesini engellemek için sealed class kullanılır
+// IdentityUser sınıfını Generic olarak kullanıyoruz
+//idendity kütüphanesini kullanmamız lazım!
+{
+    public  sealed  class AppUser: IdentityUser<Guid> 
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName => string.Join("", FirstName, LastName);
+
+
+    }
+}
+
+
---
+++ eAppointmentServer.Domain/Entities/Appointmentcs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAppointmentServer.Domain.Entities
+{
+    public sealed class Appointmentcs
+    {
+        public Appointmentcs()
+        {
+            Id= Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public Doctor? Doctor { get; set; }
+        public Guid DoctorId { get; set; }
+
+        public Patient? Patient { get; set; }
+        public Guid PatientId { get; set; }
+
+        public Guid StartDate { get; set; }
+
+        public Guid EndDate { get; set; }
+
+        public bool IsCompluted { get; set; }
+    }
+}
