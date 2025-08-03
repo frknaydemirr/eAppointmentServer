6185840aecdb7d72d955b8c4f526bfaa91372086
Furkan Aydemir <furkanaydemir12@hotmail.com>
2025-08-03 21:59:37 +0300

    continue with frontend

---
+++ eAppointmentServer.Application/Features/Doctors/GetAllDoctor/GetAllDoctorQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using eAppointmentServer.Domain.Entities;
+using eAppointmentServer.Domain.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Features.Doctors.GetAllDoctor
+{
+    internal sealed class GetAllDoctorQueryHandler(
+        IDoctorRepository doctorRepository) : IRequestHandler<GetAllDoctorQuery, Result<List<Doctor>>>
+    {
+        public async Task<Result<List<Doctor>>> Handle(GetAllDoctorQuery request, CancellationToken cancellationToken)
+        {
+
+             List<Doctor> doctors  = await doctorRepository.
+             GetAll()
+             .OrderBy(x => x.Department).
+             ThenBy(x => x.FirstName).
+             ToListAsync(cancellationToken);
+
+            return doctors;
+        }
+    }
+
+}
---
+++ eAppointmentServer.WebApi/Controllers/DoctorsController.cs
@@ -0,0 +1,22 @@
+﻿using eAppointmentServer.Application.Features.Doctors.GetAllDoctor;
+using eAppointmentServer.WebApi.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eAppointmentServer.WebApi.Controllers
+{
+    public class DoctorsController : ApiController
+    {
+        public DoctorsController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAll(GetAllDoctorQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+
+        }
+    }
+}
