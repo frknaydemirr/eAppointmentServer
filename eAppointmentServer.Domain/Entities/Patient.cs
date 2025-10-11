da76bea67918ddfb798bb97ac48e4e53ba986f37
frknaydemirr <furkanaydemir12@hotmail.com>
2025-10-11 13:02:01 +0300

    Add project files.

---
+++ eAppointmentServer.Domain/Entities/Patient.cs
@@ -0,0 +1,34 @@
+﻿namespace eAppointmentServer.Domain.Entities
+{
+    public sealed  class Patient 
+    {
+        //ıd guid tipinde olunca bizim vermemiz gereken bir değerden oluşuyor!
+      
+
+        public Patient()
+        {
+            Id=Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+
+        public string IdentityNumber { get; set; } = string.Empty;
+
+
+
+        public string City { get; set; } = string.Empty;
+
+
+        public string Town { get; set; } = string.Empty;
+
+
+        public string FullAddress { get; set; } = string.Empty;
+
+        public string FullName => string.Join("", FirstName, LastName);
+    }
+}
