073518396a9d963c178ae5bb85b495310e46b410
frknaydemirr <furkanaydemir12@hotmail.com>
2025-10-13 00:48:56 +0300

    Create Doctor  in backend size (doctor's methods)

---
+++ eAppointmentServer.WebAPI/Controllers/DoctorsControllers.cs
@@ -0,0 +1,21 @@
+﻿using eAppointmentServer.Application.Features.Doctors.GetAllDoctor;
+using eAppointmentServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eAppointmentServer.WebAPI.Controllers
+{
+    public class DoctorsControllers : ApiController
+    {
+        public DoctorsControllers(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>  GetAll(GetAllDoctorQuery request,CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
